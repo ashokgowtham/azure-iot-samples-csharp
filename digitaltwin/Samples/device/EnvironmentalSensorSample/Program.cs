ce76b9ddee7ac07f65a9f2b27f76de883f9b51d8
Ashok Gowtham M <ashokgowtham123@gmail.com>
2020-05-06 03:15:53 +0530

    [Ashok] Modify the DeviceTwin sample project to use DPS for provisioning

--- digitaltwin/Samples/device/EnvironmentalSensorSample/Program.cs
+++ digitaltwin/Samples/device/EnvironmentalSensorSample/Program.cs
@@ -5,52 +5,161 @@ using System;
 
 using Microsoft.Azure.Devices.Client;
 using Microsoft.Azure.Devices.DigitalTwin.Client;
+using Microsoft.Azure.Devices.Provisioning.Client;
+using Microsoft.Azure.Devices.Provisioning.Client.Samples;
+using Microsoft.Azure.Devices.Provisioning.Client.Transport;
+using Microsoft.Azure.Devices.Shared;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace EnvironmentalSensorSample
 {
     class Program
     {
-        // String containing Hostname, Device Id & Device Key in one of the following formats:
-        //  "HostName=<iothub_host_name>;DeviceId=<device_id>;SharedAccessKey=<device_key>"
-        //  "HostName=<iothub_host_name>;CredentialType=SharedAccessSignature;DeviceId=<device_id>;SharedAccessSignature=SharedAccessSignature sr=<iot_host>/devices/<device_id>&sig=<token>&se=<expiry_time>";
-        private static string deviceConnectionString = Environment.GetEnvironmentVariable("DIGITAL_TWIN_DEVICE_CONNECTION_STRING");
 
-        // Select one of the following transports used by DeviceClient to connect to IoT Hub.
-        private static TransportType transportType = TransportType.Mqtt;
+        // The Provisioning Hub IDScope.
+
+        // For this sample either:
+        // - pass this value as a command-prompt argument
+        // - set the DPS_IDSCOPE environment variable 
+        // - create a launchSettings.json (see launchSettings.json.template) containing the variable
+        private static string s_idScope = Environment.GetEnvironmentVariable("DPS_IDSCOPE");
+
+        // In your Device Provisioning Service please go to "Manage enrollments" and select "Individual Enrollments".
+        // Select "Add individual enrollment" then fill in the following:
+        // Mechanism: Symmetric Key
+        // Auto-generate keys should be checked
+        // DeviceID: iothubSymmetricKeydevice1
+
+        // Symmetric Keys may also be used for enrollment groups.
+        // In your Device Provisioning Service please go to "Manage enrollments" and select "Enrollment Groups".
+        // Select "Add enrollment group" then fill in the following:
+        // Group name: <your  group name>
+        // Attestation Type: Symmetric Key
+        // Auto-generate keys should be checked
+        // You may also change other enrollment group parameters according to your needs
+
+        private const string GlobalDeviceEndpoint = "global.azure-devices-provisioning.net";
+
+        //These are the two keys that belong to your enrollment group. 
+        // Leave them blank if you want to try this sample for an individual enrollment instead
+        private const string enrollmentGroupPrimaryKey = "";
+        private const string enrollmentGroupSecondaryKey = "";
+
+        //registration id for enrollment groups can be chosen arbitrarily and do not require any portal setup. 
+        //The chosen value will become the provisioned device's device id.
+        //
+        //registration id for individual enrollments must be retrieved from the portal and will be unrelated to the provioned
+        //device's device id
+        //
+        //This field is mandatory to provide for this sample
+        private static string registrationId = "";
+
+        //These are the two keys that belong to your individual enrollment. 
+        // Leave them blank if you want to try this sample for an individual enrollment instead
+        private const string individualEnrollmentPrimaryKey = "";
+        private const string individualEnrollmentSecondaryKey = "";
 
         public static int Main(string[] args)
         {
-            if (string.IsNullOrEmpty(deviceConnectionString) && args.Length > 0)
+            if (string.IsNullOrWhiteSpace(s_idScope) && (args.Length > 0))
             {
-                deviceConnectionString = args[0];
+                s_idScope = args[0];
             }
 
-            if (string.IsNullOrEmpty(deviceConnectionString))
+            if (string.IsNullOrWhiteSpace(s_idScope))
             {
-                Console.WriteLine("Please set a value for the environment variable \"DIGITAL_TWIN_DEVICE_CONNECTION_STRING\" or pass it in as an argument");
-                Console.WriteLine("Press any key to exit");
-                Console.ReadLine();
-                return 0;
+                Console.WriteLine("ProvisioningDeviceClientSymmetricKey <IDScope>");
+                return 1;
             }
 
-            using (var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, transportType))
+            string primaryKey = "";
+            string secondaryKey = "";
+            if (!String.IsNullOrEmpty(registrationId) && !String.IsNullOrEmpty(enrollmentGroupPrimaryKey) && !String.IsNullOrEmpty(enrollmentGroupSecondaryKey))
             {
-                DigitalTwinClient digitalTwinClient = new DigitalTwinClient(deviceClient);
+                //Group enrollment flow, the primary and secondary keys are derived from the enrollment group keys and from the desired registration id
+                primaryKey = ComputeDerivedSymmetricKey(Convert.FromBase64String(enrollmentGroupPrimaryKey), registrationId);
+                secondaryKey = ComputeDerivedSymmetricKey(Convert.FromBase64String(enrollmentGroupSecondaryKey), registrationId);
+            }
+            else if (!String.IsNullOrEmpty(registrationId) && !String.IsNullOrEmpty(individualEnrollmentPrimaryKey) && !String.IsNullOrEmpty(individualEnrollmentSecondaryKey))
+            {
+                //Individual enrollment flow, the primary and secondary keys are the same as the individual enrollment keys
+                primaryKey = individualEnrollmentPrimaryKey;
+                secondaryKey = individualEnrollmentSecondaryKey;
+            }
+            else
+            {
+                Console.WriteLine("Invalid configuration provided, must provide group enrollment keys or individual enrollment keys");
+                return -1;
+            }
 
-                if (digitalTwinClient == null)
+
+            using (var _security = new SecurityProviderSymmetricKey(registrationId, primaryKey, secondaryKey))
+            // Select one of the available transports:
+            // To optimize for size, reference only the protocols used by your application.
+            using (var transport = new ProvisioningTransportHandlerAmqp(TransportFallbackType.TcpOnly))
+            // using (var transport = new ProvisioningTransportHandlerHttp())
+            // using (var transport = new ProvisioningTransportHandlerMqtt(TransportFallbackType.TcpOnly))
+            // using (var transport = new ProvisioningTransportHandlerMqtt(TransportFallbackType.WebSocketOnly))
+            {
+                ProvisioningDeviceClient _provClient =
+                    ProvisioningDeviceClient.Create(GlobalDeviceEndpoint, s_idScope, security, transport);
+
+                Console.WriteLine($"RegistrationID = {_security.GetRegistrationID()}");
+                VerifyRegistrationIdFormat(_security.GetRegistrationID());
+
+                Console.Write("ProvisioningClient RegisterAsync . . . ");
+                DeviceRegistrationResult result = await _provClient.RegisterAsync().ConfigureAwait(false);
+
+                Console.WriteLine($"{result.Status}");
+                Console.WriteLine($"ProvisioningClient AssignedHub: {result.AssignedHub}; DeviceID: {result.DeviceId}");
+
+                if (result.Status != ProvisioningRegistrationStatusType.Assigned) return;
+
+                IAuthenticationMethod auth;
+                if (_security is SecurityProviderTpm)
                 {
-                    Console.WriteLine("Failed to create DeviceClient!");
-                    return 1;
+                    Console.WriteLine("Creating TPM DeviceClient authentication.");
+                    auth = new DeviceAuthenticationWithTpm(result.DeviceId, _security as SecurityProviderTpm);
+                }
+                else if (_security is SecurityProviderX509)
+                {
+                    Console.WriteLine("Creating X509 DeviceClient authentication.");
+                    auth = new DeviceAuthenticationWithX509Certificate(result.DeviceId, (_security as SecurityProviderX509).GetAuthenticationCertificate());
+                }
+                else if (_security is SecurityProviderSymmetricKey)
+                {
+                    Console.WriteLine("Creating Symmetric Key DeviceClient authenication");
+                    auth = new DeviceAuthenticationWithRegistrySymmetricKey(result.DeviceId, (_security as SecurityProviderSymmetricKey).GetPrimaryKey());
+                }
+                else
+                {
+                    throw new NotSupportedException("Unknown authentication type.");
                 }
 
-                var sample = new DigitalTwinClientSample(digitalTwinClient);
-                sample.RunSampleAsync().GetAwaiter().GetResult();
+                // using (var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, transportType))
+                using (DeviceClient iotClient = DeviceClient.Create(result.AssignedHub, auth, TransportType.Amqp))
+                {
+                    //
+                    DigitalTwinClient digitalTwinClient = new DigitalTwinClient(deviceClient);
 
-                Console.WriteLine("Waiting to receive updates from cloud...");
-                Console.WriteLine("Press any key to exit");
-                Console.ReadLine();
+                    if (digitalTwinClient == null)
+                    {
+                        Console.WriteLine("Failed to create DeviceClient!");
+                        return 1;
+                    }
+
+                    var sample = new DigitalTwinClientSample(digitalTwinClient);
+                    sample.RunSampleAsync().GetAwaiter().GetResult();
+
+                    Console.WriteLine("Waiting to receive updates from cloud...");
+                    Console.WriteLine("Press any key to exit");
+                    Console.ReadLine();
+                }
             }
-
             return 0;
         }
     }
